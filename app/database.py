9774382e46a83bdfd52aa94008fb2561447875af
Raja sekhar <rajasekhar@Rajas-MacBook-Air.local>
2026-08-10 18:51:43 +0530

    fix env loading

--- app/database.py
+++ app/database.py
@@ -1,9 +1,17 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://rajasekhar:YOUR_PASSWORD@localhost/ecommerce_db"
+# ✅ load env file
+load_dotenv()
 
-engine = create_engine(DATABASE_URL, echo=True)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set")
+
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(
     autocommit=False,
